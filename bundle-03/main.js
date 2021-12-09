839e547d02134a9fec439697a884ebd5acbf474e
Manuel Guariglia <manuelguariglia@gmail.com>
2021-12-10 00:55:57 +0100

    fix bundle-03

--- bundle-03/main.js
+++ bundle-03/main.js
@@ -87,3 +87,19 @@ console.log(dieselCars);
 
 console.log('Tutte le altre auto');
 console.log(otherCars);
+
+/* 1.1 : Questo codice inizializzato un array di oggetti(auto) tramite il metodo filter stampa array diversi suddividendo le auto per carburante utilizzato (benzina, diesel, altro). 
+*/
+
+/* 1.2 : sono presenti diversi errori di sintassi:
+- prima di dichiarare un nuovo oggetto in un array bisogna utilizzare ',' dopo aver chiuso con '{' l'ultimo oggetto dichiarato.
+
+- il metodo filterf prevede l'utilizzo di un return e di un if.
+
+*/
+
+/* 1.3 : sono presentik anche errori logici:
+prima di confrontare 'auto.type' dobbiamo trasformarlo utilizzando il metodo toLowerCase() in modo da superare problemi riguardanti le eventuali maiuscole presenti nell'oggetto.
+*/
+
+
